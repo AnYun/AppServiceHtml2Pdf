7ad41e3c764ba7cee1a506eec032ad73b0468295
AnYun <coding@anyun.idv.tw>
2018-01-29 00:25:51 +0800

    新增測試 Html 頁面和 Controller

---
+++ AppServiceHtml2Pdf/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppServiceHtml2Pdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]  // 這邊是為了範例方便，正式環境不建議這樣使用，導致所有欄位都可以讀 Html
+        public ActionResult PDF(string Html)
+        {
+            if (string.IsNullOrWhiteSpace(Html))
+            {
+                Html = System.IO.File.ReadAllText(Server.MapPath("~/App_Data/PDF.html"));
+                // 取代 {BasePath} 成實體路徑
+                Html = Html.Replace("{BasePath}", AppDomain.CurrentDomain.BaseDirectory);
+            }
+
+            var pdf = PdfHelper.ConvertHtmlTextToPDF(Html);
+
+            return File(pdf, "application/pdf");
+        }
+    }
+}
---
+++ AppServiceHtml2Pdf/Helper/PdfHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TuesPechkin;
+
+namespace AppServiceHtml2Pdf
+{
+    /// <summary>
+    /// Pdf 轉換工具
+    /// </summary>
+    public static class PdfHelper
+    {
+        /* 多執行續類型的程式需使用 ThreadSafeConverter 並且將 Converter 放置在 Static */
+        private static IConverter converter =
+            new ThreadSafeConverter(
+                new RemotingToolset<PdfToolset>(
+                    new WinAnyCPUEmbeddedDeployment(
+                        new TempFolderDeployment())));
+
+        /// <summary>
+        /// 將 Html 文字 輸出到 PDF 檔裡
+        /// </summary>
+        /// <param name="htmlText"></param>
+        /// <returns></returns>
+        public static byte[] ConvertHtmlTextToPDF(string htmlText)
+        {
+            if (string.IsNullOrEmpty(htmlText))
+            {
+                return null;
+            }
+
+            var document = new HtmlToPdfDocument
+            {
+                GlobalSettings = { },
+                Objects = {
+                    new ObjectSettings {
+                            HtmlText = htmlText
+                    }
+                 }
+            };
+
+            var result = converter.Convert(document);
+            return result;
+        }
+    }
+}
